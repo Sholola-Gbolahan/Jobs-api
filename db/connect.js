8471799b43e3c4141421d4a1ac4d220b1d9f09dc
Gbolahan <shololagbolahan@gmail.com>
2025-04-08 04:39:29 +0100

    4. Database connection - . env : added database link, while including password and database name. - App.js   - imports database connection functions   - I ensured Database get connected before running server and should throw error if not connected.

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
+    // Options to remove depreciation warnings
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
