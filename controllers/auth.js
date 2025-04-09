8075c68ffead302dd4190704c307805007c03cd7
Gbolahan <shololagbolahan@gmail.com>
2025-04-09 08:42:39 +0100

    6.1 Error Checking (Controller Example)  - Added code to check all fields are provided before making creating on database.  - returns BadRequest error if condition met  - Note - Database has it's own validator which return 500 (inter server error)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const { StatusCodes } = require("http-status-codes")
 const User = require("../models/User")
+const { BadRequestError } = require("../errors")
 
 const register = async (req, res) => {
+  // const { password, name, email } = req.body
+  // if (!password || !email || !name) {
+  //   throw new BadRequestError("Pls provide email,password and name")
+  // }
   const user = await User.create({ ...req.body })
   res.status(StatusCodes.CREATED).json(user)
 }
