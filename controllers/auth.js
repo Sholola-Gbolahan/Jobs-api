eea74d59b841609ace6a3a374d5abef0c3704dbf
Gbolahan <shololagbolahan@gmail.com>
2025-04-11 06:49:43 +0100

    8. hashing password using mongoose middleware -User.js   - added middleware funcs    - used function so as to make use of this    - this points to the created UserSchema properties    - included next( ) so as to pass to the next middleware.    - Link to doc : https://mongoosejs.com/docs/middleware.html#pre - auth.js   - removed initial hash functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,7 @@ const { BadRequestError } = require("../errors")
 const bcrypt = require("bcryptjs")
 
 const register = async (req, res) => {
-  const { password, name, email } = req.body
-  // if (!password || !email || !name) {
-  //   throw new BadRequestError("Pls provide email,password and name")
-  // }
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  const temptUser = { name, email, password: hashedPassword }
-  const user = await User.create({ ...temptUser })
+  const user = await User.create({ ...req.body })
 
   res.status(StatusCodes.CREATED).json(user)
 }
