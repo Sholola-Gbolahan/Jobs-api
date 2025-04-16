d7f3dd492c70839960b4300b3b40706c864c557a
Gbolahan <shololagbolahan@gmail.com>
2025-04-16 08:24:43 +0100

    9.1 Generate Token - Instance Method.  - User.js     - used instance method to create a function that generate token     - Doc link :  https://mongoosejs.com/docs/guide.html#methods
    - Auth.js  :
       - Assigned the created function to token variable, passing as a response.

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,12 +26,19 @@ const UserSchema = new mongoose.Schema({
     minLength: 6,
   },
 })
-
+// Password Hashing
 UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10)
   // This is point to the UserSchema properties above
   this.password = await bcrypt.hash(this.password, salt)
   next()
 })
+
+//  Function to generate User token.
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtsecret", {
+    expiresIn: "30d",
+  })
+}
 
 module.exports = mongoose.model("User", UserSchema)
