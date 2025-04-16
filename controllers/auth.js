16e2356893b0687bb4153fde04fa378122949574
Gbolahan <shololagbolahan@gmail.com>
2025-04-16 07:42:33 +0100

    9. Generate Token.  Auth.js :     - Used the lib JWT token     - Used jwt.sign to generate the token by passing required properties to it     - returned the generated token and the username.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 const { StatusCodes } = require("http-status-codes")
 const User = require("../models/User")
 const { BadRequestError } = require("../errors")
-const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
 
-  res.status(StatusCodes.CREATED).json(user)
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtscecret", {
+    expiresIn: "30d",
+  })
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
