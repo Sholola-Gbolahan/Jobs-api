7ac80d5f78e3406f4d9a9859a213224a3d07ea89
Gbolahan <shololagbolahan@gmail.com>
2025-04-21 08:40:25 +0100

    31-32 . Security Setup done - installed and setup some security packages in the app to protect from attackers.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require("dotenv").config()
 require("express-async-errors")
-const express = require("express")
 
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+const express = require("express")
 const app = express()
 
 // import connectDB
@@ -27,6 +33,18 @@ app.use("/api/v1/jobs", authenticationUser, jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
+app.set("trust proxy", 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 const port = process.env.PORT || 3000
 
