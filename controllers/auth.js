0af6966c56c74fc7072086cda0ad2ad466bb536c
Gbolahan <shololagbolahan@gmail.com>
2025-05-06 22:54:28 +0100

    Fixed Register User if already exit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,38 @@
-const { StatusCodes } = require("http-status-codes")
-const User = require("../models/User")
-const { BadRequestError, UnauthenticatedError } = require("../errors")
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body })
-  const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
-}
+  const user = await User.create({ ...req.body });
+
+  const userExit = await User.findOne({ email });
+  if (userExit) {
+    throw new BadRequestError("User Already Exit");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 
 const login = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new BadRequestError("Please provide email and password")
+    throw new BadRequestError("Please provide email and password");
   }
 
-  const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
   if (!user) {
-    throw new UnauthenticatedError("Invalid Credential")
+    throw new UnauthenticatedError("Invalid Credential");
   }
 
-  const isPasswordCorrect = await user.comparePassword(password)
+  const isPasswordCorrect = await user.comparePassword(password);
 
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError("Invalid Credential")
+    throw new UnauthenticatedError("Invalid Credential");
   }
 
-  const token = user.createJWT()
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
-}
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
-module.exports = { register, login }
+module.exports = { register, login };
